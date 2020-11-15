22cdb4d0552df428f43438b2264f624b6eaef4dd
schiderf <felix.schider02@htl-salzburg.ac.at>
2020-11-15 23:24:26 +0100

    create first version of the script

--- Assets/MyGame/Scripts/GameScript.cs
+++ Assets/MyGame/Scripts/GameScript.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    int max;
+    int min;
+    int guess;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        max = 100;
+        min = 1;
+        guess = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            guessNext();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            guessNext();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("correct Guess");
+        }
+    }
+
+    void guessNext()
+    {
+        guess = (min + max) / 2;
     }
 }
